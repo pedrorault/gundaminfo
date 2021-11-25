51d8776f6a84e7a959cd52b399e512cb80b607ab
pedrorault <pedrorault@gmail.com>
2021-11-24 22:12:18 -0300

    Fix GET /login spec test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,25 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /new" do
+  describe "GET /login" do
     it "returns http success" do
-      get "/sessions/new"
+      get "/login"
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/sessions/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/sessions/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
