5f6080fed61e975fa0304c3e8042f61baeca3007
pedrorault <pedrorault@gmail.com>
2021-12-08 20:35:19 -0300

    Telas de comparacao

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get '/login', to: 'sessions#new'
   post'/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
- 
+  get '/logout', to: 'sessions#destroy'  
+  get '/comparacao', to: 'toys#compare' 
 end
 # https://guides.rubyonrails.org/routing.html
